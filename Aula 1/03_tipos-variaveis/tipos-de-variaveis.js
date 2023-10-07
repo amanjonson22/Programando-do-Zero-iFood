4e0a28dbd26f66bb692fd0a3c5a237d3babdc450
Amanda Caroline <amandacsr10@gmail.com>
2023-10-07 16:20:08 -0300

    edit tipos-de-variaveis

---
+++ Aula 1/03_tipos-variaveis/tipos-de-variaveis.js
@@ -0,0 +1,6 @@
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let vivoMorto = false
+let vidaPokemon = 45
+
+console.log("Pokemon: " + nomePokemon + ", Lvl: " + nivelPokemon + ", Está vivo? " + vivoMorto + ", PV: " + vidaPokemon)
