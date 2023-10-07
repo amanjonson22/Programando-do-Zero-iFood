6406a92a5f393b5c7ebe331c7744c951ed473f4e
Amanda Caroline <amandacsr10@gmail.com>
2023-10-07 16:02:37 -0300

    edit constante

--- constantes/constantes.js
+++ constantes/constantes.js
@@ -1,5 +1,5 @@
+// constante não pode mudar o valor. utiliza quando o valor for repetitivo.
 const notificacao = "Pokemon Go diz:"
-
 
 //saida
 console.log(texto + " tem um novo pokemon na região")
