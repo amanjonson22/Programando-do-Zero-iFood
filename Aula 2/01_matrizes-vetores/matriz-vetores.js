c8209231dd1db78f48511748413ca98a21b51d8e
Amanda Caroline <amandacsr10@gmail.com>
2023-10-07 17:45:30 -0300

    create file with study about lists

---
+++ Aula 2/01_matrizes-vetores/matriz-vetores.js
@@ -0,0 +1,20 @@
+// palavra-chave da aula de matriz = coleção
+let nomePessoa = ["Amanda", "Carolina", "Juliana", "Felipe", "Roberto", "Alex"]
+console.log(nomePessoa[0])
+console.log(nomePessoa[4])
+
+console.log("tamanho da lista: " + nomePessoa.length)
+
+nomePessoa.pop()
+
+console.log("tamanho da lista: " + nomePessoa.length)
+
+nomePessoa.reverse()
+
+console.log(nomePessoa)
+
+nomePessoa.shift()
+
+console.log(nomePessoa)
+
+//evitar ter vetores que guardam tipos diferentes.
---
+++ Aula 2/02_trabalhando-com-vetores/trabalhando-com-vetores.js
@@ -0,0 +1,11 @@
+// matrizes são bidimensionais
+let nomePessoa = ["Fernanda Machado"]
+let pessoas = [
+    ["Fernanda Machado", 15, "Dança"],
+    ["Francisco de Abreu", 16, "Vôlei"],
+    ["Alberto Chagas", 19, "Balé"]
+]
+
+console.log("nome: " + pessoas[0][0])
+console.log("idade: " + pessoas[0][1])
+console.log("esporte: " + pessoas[0][2])
