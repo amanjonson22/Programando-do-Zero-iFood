2c83c09bda20856d8279eaaea0629fb28194552b
Amanda Caroline <amandacsr10@gmail.com>
2023-10-07 16:45:18 -0300

    edit exercicio2 e exercicio3

---
+++ Aula 1/04_exercicio/exercicio2.js
@@ -0,0 +1,11 @@
+// nome, idade, numero de telefone, cpf, endereço, recebe pensão
+// string = armazenar texto
+// number = armazenar numeros
+// boolean = armazenar valores lógicos
+
+let nomePessoa = "Alberto Assis"
+let idade = 55
+let numeroTelefone = "+55 (12) 98805-2341"
+let cpf = "358.231.345-22"
+let endereco = "Rua Machado de Assis, n 1023, bairro Esplanada"
+let recebePensao = true
---
+++ Aula 1/04_exercicio/exercicio3.js
@@ -0,0 +1,50 @@
+// objeto - iPad
+const marca_iPad = "Apple"
+let modelo = "iPad 11"
+let temCaneta = true
+let memoria = 128
+let cor = "prata"
+let idioma = "PT-BR"
+
+// objeto - janela
+let fechada = true
+let cortina = true
+let vidro = true
+let fosca = false
+let rede = true
+
+// objeto - caneta
+let corTinta = "azul"
+let marcaCaneta = "COPIC"
+let ponta = "dupla"
+let quantidadeTinta = 100
+let tampa = true
+
+// objeto - cama
+let modeloCama = "Queen"
+let colchao = "Colchão ortopédico"
+let pes = true
+let altura = 78
+let comprimento = 300
+let largura = 300
+let marcaCama = "Rainhas"
+
+// pessoa - IMC
+let nomePessoa_IMC = "Annelise"
+let peso_IMC = 48
+let altura_IMC = 1.56
+let IMC = peso_IMC/(altura_IMC**2)
+
+// objeto - avião
+let marca_aviao = "Cessna 150"
+let motorQuantidade = 1
+let modeloMotor = "Continental O-200-A"
+let potenciaMotor = 100
+let velocidadeMaxima = 109
+let passageiros = 0
+let tetoMaximo = 14000
+let areaAsa = 15
+let envergadura = 10.11
+let comprimento_aviao = 7.29
+
+console.log(marca_aviao + ": " + "quantidade de motores: " + motorQuantidade + "; modelo do motor: " + modeloMotor + "; potência do motor: " + potenciaMotor + "; velocidade máxima: " + velocidadeMaxima + "; quantidade de pax: " + passageiros + "; teto máximo: " + tetoMaximo + "área da asa: " + areaAsa + "; envergadura: " + envergadura + "; comprimento do avião: " + comprimento_aviao)
