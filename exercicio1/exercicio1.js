9cd5b69461b0525a86bbba7fac0385d982b0d17d
Amanda Caroline <amandacsr10@gmail.com>
2023-10-07 16:09:38 -0300

    create exercicio1

---
+++ exercicio1/exercicio1.js
@@ -0,0 +1,12 @@
+let poteCafe = "café pilão"
+let poteAcucar = "açúcar crystal"
+let poteBiscoito = "biscoito de chocolate"
+const mensagemVovó = "Os potes da vovó tem: "
+
+console.log(mensagemVovó + poteCafe + ", " + poteAcucar + ", " + poteBiscoito)
+
+poteCafe = "Café do Padre"
+poteAcucar = "Açúcar Mascavo"
+poteBiscoito = "Biscoito de Baunilha"
+
+console.log(mensagemVovó + poteCafe + ", " + poteAcucar + ", " + poteBiscoito)
