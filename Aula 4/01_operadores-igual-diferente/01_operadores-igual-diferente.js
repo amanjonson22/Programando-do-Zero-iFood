7ceeb0dd27aa9160966cddef417269e0f46f60f1
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 18:18:10 -0300

    create operadores igual ou diferente

---
+++ Aula 4/01_operadores-igual-diferente/01_operadores-igual-diferente.js
@@ -0,0 +1,15 @@
+// operadores de igual
+
+// = é atribuição
+let numero = 1
+console.log(numero)
+
+numero = 5
+console.log(numero)
+
+// == é para comparar o valor
+console.log("número é igual a 5? " + numero == 5 + "  número é igual a 1? " + numero == 1)
+
+// === é para comparar o valor e o formato
+console.log(numero === "5")
+console.log(numero == "5")
