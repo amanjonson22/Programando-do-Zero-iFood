a114a9387e6471a5d3d61d24447ff59b909e6c1c
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 11:30:51 -0300

    create exercicio plantas

---
+++ Aula 2/03_exercicios/plantas.js
@@ -0,0 +1,25 @@
+let plantas = [
+    ["Morango", "Temperado", "60 a 80 dias"],
+    ["Alface", "Temperado", "60 a 70 dias"],
+    ["Couve", "Tropical", "10 a 16 semanas"],
+    ["Coentro", "Tropical","30 a 70 dias"]]
+    
+const aPlanta = "A planta: "
+const clima = "É de clima: "
+const colheita = "Pode colher após: "
+
+console.log(aPlanta + plantas[0][0])
+console.log(clima + plantas[0][1])
+console.log(colheita + plantas[0][2])
+
+console.log()
+
+console.log(aPlanta + plantas[1][0])
+console.log(clima + plantas[1][1])
+console.log(colheita + plantas[1][2])
+
+console.log()
+
+console.log(aPlanta + plantas[2][0])
+console.log(clima + plantas[2][1])
+console.log(colheita + plantas[2][2])
