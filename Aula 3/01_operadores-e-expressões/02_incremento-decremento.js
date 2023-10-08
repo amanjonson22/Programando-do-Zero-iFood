c1fed13952ff42a4fe2353202cec3e5a7cba074b
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 18:09:03 -0300

    edit incremento e decremento - adicionado comando while usando chatgpt

---
+++ Aula 3/01_operadores-e-expressões/02_incremento-decremento.js
@@ -0,0 +1,12 @@
+// incremento e decremento
+let contador = 1
+while (contador < 20){
+    contador++
+    console.log("Contador: " + contador)
+}
+
+// decremento
+while (contador > 0){
+    contador--
+    console.log("Contador: " + contador)
+}
