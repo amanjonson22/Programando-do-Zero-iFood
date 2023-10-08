d28a432265445561808bdbe02b54dff34c099942
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 14:48:31 -0300

    edit exemplo02

--- Aula 3/01_operadores-e-expressões/02_exemplo02.js
+++ Aula 3/01_operadores-e-expressões/02_exemplo02.js
@@ -2,3 +2,25 @@ let codigoDoProduto = 1023
 let codigoExato = codigoDoProduto - 1000
 
 console.log(codigoExato)
+
+let precoProduto = 100.99
+let taxa = 3.2
+let valorComTaxa = precoProduto*taxa
+
+console.log = valorComTaxa
+
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador*multiplicando
+
+console.log("O resultado da multiplicação é: " + produto)
+
+multiplicador = 5
+produto = multiplicador*multiplicando
+
+let notaMercado = 50
+let pessoasParaDividir = 2
+console.log("operação de divisão: " + notaMercado/pessoasParaDividir)
+
+let calculo = 10%3
+console.log("Operação de módulo: " + calculo)
