aa0d13799885ea5ffc3f00ccc515ee63c1462b6f
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 18:29:11 -0300

    create operadores de comparação

--- Aula 4/01_operadores-igual-diferente/01_operadores-igual-diferente.js
+++ Aula 4/01_operadores-igual-diferente/01_operadores-igual-diferente.js
@@ -13,3 +13,7 @@ console.log("número é igual a 5? " + numero == 5 + "  número é igual a 1? " 
 // === é para comparar o valor e o formato
 console.log(numero === "5")
 console.log(numero == "5")
+
+// operadores de diferente
+console.log("numero é diferente de 5? " + numero !== 5)
+console.log("numero é diferente de 1? " + numero !== 1)
---
+++ Aula 4/02_operadores-de-comparação/01_operadores-comparacao.js
@@ -0,0 +1,16 @@
+// operadores de comparação
+let idadeMinima = 18
+let idadeUsuario = 17
+
+console.log(idadeUsuario > idadeMinima)
+
+idadeUsuario = 18
+console.log(idadeUsuario >= idadeMinima)
+
+let idadeCorte = 50
+idadeUsuario = 45
+
+console.log(idadeUsuario < idadeCorte)
+
+idadeUsuario = 50
+console.log(idadeUsuario <= idadeCorte)
