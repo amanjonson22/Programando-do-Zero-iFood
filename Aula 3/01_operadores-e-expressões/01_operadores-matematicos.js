1d3ab9126b6f21f2a037b9b402d22d592fca642b
Amanda Caroline <amandacsr10@gmail.com>
2023-10-08 13:10:15 -0300

    create operadores matematicos

---
+++ Aula 3/01_operadores-e-expressões/01_operadores-matematicos.js
@@ -0,0 +1,12 @@
+// operadores aritméticos
+let idade = 20
+
+console.log("Valor da minha variavel: " + idade)
+
+idade += 6
+
+console.log("Operaçãp de adição: " + idade)
+
+idade -= 2
+
+console.log("Operação de subtração: " + idade)
---
+++ Aula 3/01_operadores-e-expressões/02_exemplo02.js
@@ -0,0 +1,4 @@
+let codigoDoProduto = 1023
+let codigoExato = codigoDoProduto - 1000
+
+console.log(codigoExato)
